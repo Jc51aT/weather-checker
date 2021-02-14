aba5f963b13681a0b2b7927346d2b00f0b9bb676
Jc51aT <32559153+Jc51aT@users.noreply.github.com>
2021-02-14 18:55:47 -0500

    rebased commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import WeatherResults from './components/WeatherResults';
 
-function App() {
+// f9a9d367e665d5c38c32cefaf38d705b
+const  App = () => {
+  const [resultsWeather, setResultsWeather] = useState([]);
+  const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+  //fetch weather results
+  const fetchWeather = async (city) => {
+    
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    
+    const data = await res.json();
+    console.log(data);
+    setResultsWeather(data);
+
+  }
+
+  //fetchWeather('place');
+  console.log(Object.keys(resultsWeather).length);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header onSearch={fetchWeather}/>
+      {Object.keys(resultsWeather).length > 0 && <WeatherResults results={resultsWeather}/>}
     </div>
   );
 }
